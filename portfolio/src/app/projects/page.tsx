b1cfbd4a27c16867011f012cedb78e8c1ca458a7
venkatesh2100 <venkyvenkat.iit@gmail.com>
2024-10-11 21:05:23 +0530

    push

--- portfolio/src/app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ProjectComponent } from "../../../components/ui/projects";
-
-export default function Projectspage() {
-  return (
-    <div className="min-h-screen md:mx-60">
-      <ProjectComponent />
-    </div>
-  );
-}
--- portfolio/src/app/resume/page.tsx
+++
@@ -1,9 +0,0 @@
-import ResumeComponent from "../../../components/ui/resume";
-
-
-
-export default function ResumePage(){
-  return(
-    <div className="min-h-screen  md:mx-60"><ResumeComponent/></div>
-  )
-}
