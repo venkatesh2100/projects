c03cd61a79a1c473fd653ca892dad4457b12ac96
venkatesh2100 <venkyvenkat.iit@gmail.com>
2024-10-22 22:20:59 +0530

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "../../components/navbar";
+import Navbar from "../../components/navbar"
 import Footer from "../../components/footer";
 import { Toaster } from "sonner";
 
@@ -29,10 +29,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialia0sed`}
+        className={`${geistSans.variable} ${geistMono.variable} antialia0sed dark:bg-black`}
       >
         <Toaster richColors/>
-        <Navbar />
+        <Navbar  />
         {children}
         <Footer/>
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,18 @@
+
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode:'class',
+  darKMode:'class',// Enable dark mode
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
     },
   },
   plugins: [],
 };
+
 export default config;
