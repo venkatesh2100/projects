9df828a8702723d31448ba8fe89db2d39bb453a0
venkatesh2100 <venkyvenkat.iit@gmail.com>
2024-10-30 00:47:57 +0530

    v.3.0

---
+++ portfolio/components/aboutme/ui/MagicButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MagicButton = ({ text, url }: { text: string; url: string }) => {
+  return (
+    <a href={url} target="_blank">
+      <button className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none ">
+        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+
+        <span
+          className=" inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
+             bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 "
+        >
+          {text}
+        </span>
+      </button>
+    </a>
+  );
+};
+
+export default MagicButton;
--- portfolio/src/app/aboutme/page.tsx
+++ portfolio/src/app/aboutme/page.tsx
@@ -1,15 +1,37 @@
-import { Me } from "../../../components/aboutme/aboutme";
+"use client";
+// import { Me } from "../../../components/aboutme/aboutme";
 import Particles from "../../../components/aboutme/particles";
 
 
 
 
-export default function Aboutme() {
-  return (
-    <div className="min-h-screen">
-<Particles/>
-      <Me/>
-    </div>
-  )
 
+import Hero from "../../../components/aboutme/Hero"
+import Projects from "../../../components/aboutme/Projects";
+// import Skills from "../../../components/aboutme/particles";
+import { FloatingNav } from "../../../components/aboutme/ui/FloatingNavbar";
+import { navItems } from "../../../components/aboutme/data";
+// import { Timeline } from "../../../components/aboutme/ui/Timeline";
+// import Education from "../../../components/aboutme/Education";
+// import Links from "./componens/Links";
+import Contact from "../../../components/aboutme/Contact";
+// import Navbar from "../../../components/navbar";
+// import { ModeToggle } from "./components/ui/ModeToggle";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 text-text-color flex justify-center items-center flex-col min-h-screen overflow-hidden ">
+      <div className=" w-full">
+        {/* <Navbar/> */}
+        {/* <ModeToggle /> */}
+        <Particles/>
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        {/* <Skills /> */}
+        <Projects />
+        {/* <Education /> */}
+        <Contact />
+      </div>
+    </main>
+  );
 }
