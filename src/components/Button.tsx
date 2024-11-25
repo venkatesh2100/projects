8b46bc992e0f7bf292bcf134524b28ad15ff8dca
Ajeet Kumar <ajeet62068bca@gmail.com>
2024-11-26 03:38:44 +0530

    fixed the error

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,17 @@
-export function Button({text}: {text: string}){
-  return <div className="mt-10">
-      <button className="bg-[#FF7119] text-white font-medium px-11 py-4 rounded-tr-full rounded-tl-none rounded-bl-full rounded-br-full hover:shadow-lg">
-          {text}
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+}
+
+export function Button({ text, onClick }: ButtonProps) {
+  return (
+    <div className="mt-10">
+      <button
+        className="bg-[#FF7119] text-white font-medium px-11 py-4 rounded-tr-full rounded-tl-none rounded-bl-full rounded-br-full hover:shadow-lg"
+        onClick={onClick}
+      >
+        {text}
       </button>
-  </div>
+    </div>
+  );
 }
