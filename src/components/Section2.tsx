6e41cc217daf91be95a99f3f02c2bef96e9c2608
Ajeet Kumar <ajeet62068bca@gmail.com>
2024-11-26 00:29:27 +0530

    done some changes

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -8,12 +8,12 @@ const FeatureCircle: React.FC = () => {
         <img
           src="https://bambino.bold-themes.com/demo-01/wp-content/uploads/sites/2/2022/07/image_01_home_04-600x540.png"
           alt="Center Content"
-          className="h-96 bg-none"
+          className="w-96"
         />
       </div>
 
       {/* Features */}
-      <div className="relative w-full h-auto mt-[-220px] flex flex-wrap justify-center lg:justify-between space-x-4">
+      <div className="relative w-full h-auto mt-[-220px] flex flex-col justify-center lg:w-[800px] lg:h-[800px] lg:flex-none">
         {/* Feature 1 */}
         <div className="flex flex-col items-center text-center rounded-full shadow-lg w-56 h-56 p-6 bg-green-300 mb-8 lg:absolute lg:top-[10px] lg:left-[310px]">
           <div className="text-4xl">🫙</div>
@@ -31,7 +31,7 @@ const FeatureCircle: React.FC = () => {
         </div>
 
         {/* Feature 3 */}
-        <div className="flex flex-col items-center text-center rounded-full shadow-lg w-56 h-56 p-6 bg-orange-300 mb-8 lg:absolute lg:top-[550px] lg:left-[640px]">
+        <div className=" flex flex-col items-center text-center rounded-full shadow-lg w-56 h-56 p-6 bg-orange-300 lg:absolute lg:top-[550px] lg:left-[640px]">
           <div className="text-4xl">🍎</div>
           <p className="mt-4 text-base font-semibold text-white">
             100% organic ingredients
@@ -39,7 +39,7 @@ const FeatureCircle: React.FC = () => {
         </div>
 
         {/* Feature 4 */}
-        <div className="flex flex-col items-center text-center rounded-full shadow-lg w-56 h-56 p-6 bg-orange-400 mb-8 lg:absolute lg:top-[780px] lg:left-[310px]">
+        <div className=" flex flex-col items-center text-center rounded-full shadow-lg w-56 h-56 p-6 bg-orange-400 mb-8 lg:absolute lg:top-[780px] lg:left-[310px]">
           <div className="text-4xl">♻️</div>
           <p className="mt-4 text-base font-semibold text-white">
             100% recyclable packaging
