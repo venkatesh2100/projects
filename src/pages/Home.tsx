d960f5b64e6e514bdf3794c96c00130d6c24f431
Ajeet Kumar <ajeet62068bca@gmail.com>
2024-11-25 10:57:11 +0530

    added more section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import { SectionProducts } from "../components/Section3";
 import { Footer } from "../components/Footer";
 import { Farm } from "../components/Section4";
 import { SectionReviews } from "../components/SectionReviews";
+import { Section5 } from "../components/Section5";
 
 export function Home() {
 
@@ -16,6 +17,7 @@ export function Home() {
       <SectionProducts/>
       <Farm/>
       <SectionReviews/>
+      <Section5/>
       <Footer/>
     </div>
   )
