1d3b7b2326d67324621511bc171132ff4489f165
venkatesh2100 <venkyvenkat.iit@gmail.com>
2024-12-01 21:59:53 +0530

    up

--- src/components/SectionReviews.tsx
+++ src/components/SectionReviews.tsx
@@ -7,11 +7,11 @@ export function SectionReviews() {
             <div
                 className="bg-backgroundColor bg-no-repeat bg-center"
                 style={{
-                    backgroundImage: `url('/public/background_02_home_04.png')`,
+                    backgroundImage: `url('/background_02_home_04.png')`,
                 }}
             >
                 {/* Responsive Background Sizing */}
-                <div className="w-full min-h-[900px] lg:min-h-[950px] flex flex-col items-center justify-center py-10 
+                <div className="w-full min-h-[900px] lg:min-h-[950px] flex flex-col items-center justify-center py-10
                     bg-[length:150%] md:bg-[length:120%] lg:bg-[length:100%]">
                     {/* Feedback Card */}
                     <div className="max-w-4xl w-full px-4">
