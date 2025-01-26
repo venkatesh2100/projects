00f6294775e7f519d58c310424c89699e0c2d500
venkatesh2100 <venkyvenkat.iit@gmail.com>
2025-01-26 16:12:55 +0530

    Del portfolio

--- portfolio/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-import { SocialLink } from "./ui/socialink";
-export default function Footer() {
-  return (
-    <div className="flex flex-col  items-center mt-4 pt-2">
-      <SocialLink />
-      <div>© 2024<span className="underline  text-blue-500">Venky</span>, All rights reserved</div>
-    </div>
-  );
-}
