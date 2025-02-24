537331d3b48484dd7a5c59c30bfe710c8caf5e81
venkatesh2100 <venkyvenkat.iit@gmail.com>
2025-02-24 22:16:33 +0530

    mono-repo

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,16 @@
+// import {Button} from "@repo/ui/button"
+// import {Admin} from "@repo/ui/admin"
+import Admin from "@repo/ui/admin"
+export default function Home() {
+  return (
+    <div className="flex p-20">
+      <div className="text-4xl"> Hello Im venky COOL</div>
+      <p> Doing Job Great. Again Strated Building Projects From Scratch</p>
+      {/* <Button/> */}
+      <div className="">
+      <Admin/>
+      </div>
+
+    </div>
+  );
+}
