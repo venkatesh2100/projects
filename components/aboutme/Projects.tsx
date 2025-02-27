43863ab4c4e8d3fc68a422d821240a10921f5ac6
venkatesh2100 <venkyvenkat.iit@gmail.com>
2025-02-27 10:15:11 +0530

    dukkan update

--- components/aboutme/Projects.tsx
+++ components/aboutme/Projects.tsx
@@ -38,7 +38,7 @@ const Projects = () => {
               </h1>
 
               <p
-                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                className="lg:text-xl  lg:font-normal font-light text-sm line-clamp-2"
                 style={{
                   color: '#BEC1DD',
                   margin: '1vh 0',
