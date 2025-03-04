8d2a6a606596743d8ca215bfb7c99778a7ab7531
venkatesh2100 <venkyvenkat.iit@gmail.com>
2025-03-04 22:49:08 +0530

    New navbar

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/navbar"
+
+
+export default function Home() {
+  return(
+    <div>
+      <Navbar/>
+      <div className="pt-20 text-4xl">Home Page</div>
+    </div>
+  )
+}
